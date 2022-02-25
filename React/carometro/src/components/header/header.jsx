0df07556633b2b7f2aa10e0ff9e2eebbf9771484
Kaue Camargo Marinho <48038990889@info.scs>
2022-02-25 17:07:51 -0200

    Home iniciada

--- React/carometro/src/components/header/header.jsx
+++ React/carometro/src/components/header/header.jsx
@@ -4,14 +4,17 @@ import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button'
 
 export default function Header() {
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="fixed">
-        <Toolbar>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-          </Typography>
+      <AppBar position="fixed" sx={{
+        backgroundColor: "#3B86CC"
+      }}>
+        <Toolbar sx={{width: '90%'}}>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}></Typography>
+          <Button color="inherit">Sair</Button>
         </Toolbar>
       </AppBar>
     </Box>
