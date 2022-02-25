6cc482e1c2f650039d6a68f9ff21c51efbe11541
Edson Júnior <edson.vale@portalsesisp.org.br>
2022-02-25 15:43:38 -0300

    Projeto iniciado

--- React/carometro/src/pages/login/login.jsx
+++ React/carometro/src/pages/login/login.jsx
@@ -1,6 +1,23 @@
 import React from "react";
+import { useState } from "react";
+import {useHistory} from "react-router-dom";
 
-export default function Footer() {
+
+export default function Login() {
+    const[email, setEmail] = useState('');
+    const[senha, setSenha] = useState('');
+    const [isLoading, setIsLoading] = useState(false);
+    const [erroMensagem, setErroMensagem] = useState('');
+
+    let history = useHistory();
+
+    function logar(event) {
+        event.preventDefault();
+        
+        setErroMensagem('')
+        setIsLoading(true)
+    }
+
     return(
         <div>
             <h1>Login</h1>
